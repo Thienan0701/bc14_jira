4ebd30526023eb54128857d9c1728e86ed4ddde5
dovanthao2404 <dovanthao2404@gmail.com>
2022-01-04 21:12:41 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,47 +1,17 @@
 import "./App.css";
 
-import { lazy } from "react";
-
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { HomeTemplate } from "./templates/HomeTemplate/HomeTemplate";
 
-import { UserLoginTemplate } from "./templates/LoginTemplate/UserLoginTemplate";
 import DrawerCommon from "./components/DrawerCommon/DrawerCommon";
 import PageNotFound from "./templates/PageNotFound/PageNotFound";
+import { renderRouteHome, renderRouteUser } from "./routes";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <HomeTemplate
-          path="/"
-          exact
-          Component={lazy(() => import("./pages/Home/Home"))}
-        />
-        <HomeTemplate
-          path="/project-management"
-          Component={lazy(() => import("./pages/Home/Home"))}
-        />
-        <HomeTemplate
-          path="/user-manage"
-          Component={lazy(() => import("./pages/UserManage/UserManage"))}
-        />
-        <HomeTemplate
-          path="/project-detail/:id"
-          Component={lazy(() => import("./pages/ProjectDetail/ProjectDetail"))}
-        />
-        <HomeTemplate
-          path="/profile"
-          Component={lazy(() => import("./pages/Profile/Profile"))}
-        />
-        <UserLoginTemplate
-          path="/login"
-          Component={lazy(() => import("./pages/Login/Login"))}
-        />
-        <UserLoginTemplate
-          path="/register"
-          Component={lazy(() => import("./pages/Register/Register"))}
-        />
+        {renderRouteHome()}
+        {renderRouteUser()}
         <Route path="*" component={PageNotFound} />
       </Switch>
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,70 @@
-export const routeHome = [];
+import { lazy } from "react";
+import { HomeTemplate } from "../templates/HomeTemplate/HomeTemplate";
+import { UserLoginTemplate } from "../templates/LoginTemplate/UserLoginTemplate";
 
-export const routeAdmin = [];
+const routeHome = [
+  {
+    path: "/",
+    Component: lazy(() => import("../pages/Home/Home")),
+    exact: true,
+  },
+  {
+    path: "/project-management",
+    Component: lazy(() => import("../pages/Home/Home")),
+    exact: true,
+  },
+  {
+    path: "/user-manage",
+    Component: lazy(() => import("../pages/UserManage/UserManage")),
+    exact: true,
+  },
+  {
+    path: "/project-detail/:id",
+    Component: lazy(() => import("../pages/ProjectDetail/ProjectDetail")),
+    exact: true,
+  },
+  {
+    path: "/profile",
+    Component: lazy(() => import("../pages/Profile/Profile")),
+    exact: true,
+  },
+];
+
+const routeUser = [
+  {
+    path: "/login",
+    Component: lazy(() => import("../pages/Login/Login")),
+    exact: true,
+  },
+  {
+    path: "/register",
+    Component: lazy(() => import("../pages/Register/Register")),
+    exact: true,
+  },
+];
+
+export const renderRouteHome = () => {
+  return routeHome.map((route, key) => {
+    return (
+      <HomeTemplate
+        key={key}
+        exact={route.exact}
+        Component={route.Component}
+        path={route.path}
+      />
+    );
+  });
+};
+
+export const renderRouteUser = () => {
+  return routeUser.map((route, key) => {
+    return (
+      <UserLoginTemplate
+        key={key}
+        exact={route.exact}
+        Component={route.Component}
+        path={route.path}
+      />
+    );
+  });
+};
